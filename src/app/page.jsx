38d4aab55e179327f7e8d82f6cc632a50fec343a
zalish-shawon <zalish.shawon@gmail.com>
2024-06-26 20:39:29 +0600

    added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+"use client";
+
+import React, { useState } from 'react';
 
 const HomePage = () => {
+  const [counter, setCounter] = useState(0);
   return (
-    <div>
-        <h1>Hello world</h1>
+    <div className='max-w-[85%] mx-auto mt-20'>
+        <h1 className='mb-10'>Counter App</h1>
+        <button className='bg-cyan-500 p-10' onClick={()=>  setCounter(counter+1)}>Increase</button>
+        <button className='bg-cyan-500 p-10 ml-5' onClick={()=> setCounter(counter-1)}>Decrease</button>
+
+        <h1>Counter: {counter}</h1>
     </div>  
   );
 };
